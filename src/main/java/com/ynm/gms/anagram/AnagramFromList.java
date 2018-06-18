1de814daee414957254561d2a1aa2ee02344bd4b
Yogesh <yogesh.manware@team.telstra.com>
2018-06-18 12:37:11 +1000

    added slf4j/log4j2

--- src/main/java/com/ynm/gms/anagram/AnagramFromList.java
+++ src/main/java/com/ynm/gms/anagram/AnagramFromList.java
@@ -1,5 +1,9 @@
 package com.ynm.gms.anagram;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -7,13 +11,19 @@ import java.util.stream.Collectors;
  * @auther Yogesh Manware
  */
 public class AnagramFromList {
+    private static Logger logger = LoggerFactory.getLogger(AnagramFromList.class);
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.addAll(Arrays.asList("eat", "tea", "pot", "top", "tie"));
-        System.out.println(groupAnagrams(list));
-        list.addAll(Arrays.asList("lol", "llo", "rat", "tar","art","kart"));
-        System.out.println(groupAnagrams2(list));
+        Collection<List<String>> resultList = groupAnagrams(list);
+        logger.info("List of words {} ", list);
+        logger.info("Anagram View of the given list {}", resultList.toString());
+        list.clear();
+        list.addAll(Arrays.asList("lol", "llo", "rat", "tar", "art", "kart"));
+        Collection<Set<String>> resultList2 = groupAnagrams2(list);
+        logger.info("List of words {} ", list);
+        logger.info("Anagram View of the given list {}", resultList2.toString());
     }
 
     //return collection of lists of strings
@@ -26,11 +36,11 @@ public class AnagramFromList {
     }
 
     //return sets of sets of strings
-    public static Set<Set<String>> groupAnagrams2(List<String> words) {
+    public static Collection<Set<String>> groupAnagrams2(List<String> words) {
         return words.stream().collect(Collectors.groupingBy(w -> {
             char[] chars = w.toCharArray();
             Arrays.sort(chars);
             return new String(chars);
-        }, Collectors.toSet())).values().stream().collect(Collectors.toSet());
+        }, Collectors.toSet())).values();
     }
 }
